69ea232bfed227c11b77ea3f5ac216d5c010c795
201816040314 <56504172+201816040314@users.noreply.github.com>
2019-11-01 13:09:26 +0800

    Update Invoice.cpp
    This is my assignment_03
    Student ID:201816040314

--- 201816040314/Ex03_13/Invoice.cpp
+++ 201816040314/Ex03_13/Invoice.cpp
@@ -1,28 +1,64 @@
-#ifndef INVOICE_H_INCLUDED
-#define INVOICE_H_INCLUDED
-#include <iostream>
-using namespace std;
-class Invoice//declare a class
+#include "Invoice.h"
+Invoice::Invoice(string id,string discription,int sale,int price)//the constructor with three parameter
 {
-public:
-    Invoice(string,string,int,int);//the constructor with three parameter
-    void setPartId(string);// the function is to set the part id
-    void setPartDis(string);// the function is to set the part description
-    void setAllSale(int);// the function is to set the sale number
-    void setSinglePrice(int);// the function is to set the single price
-    void display();// the function is to display the content
-    string getPartId();// the function is to get the part id
-    string getPartDis();// the function is to set the part description
-    int getAllSale();// the function is to get the sale number
-    int getSinglePrice();// the function is to get the single price
-    int getInvoiceAmount();// the function is to get the invoice amount
+    setPartDis(discription);
+    setPartId(id);
+    setAllSale(sale);
+    setSinglePrice(price);
+}
+void Invoice::setPartDis(string discription)// the function is to set the part description
+{
+    partDis=discription;
+}
+void Invoice::setPartId(string id)// the function is to set the part id
+{
+    partId=id;
+}
+void Invoice::setSinglePrice(int price)// the function is to set the single price
+{
+    if(price>0)
+    {
+       singlePrice=price;
+    }
+    else
+        singlePrice=0;
 
-private:
-    string partId;
-    string partDis;
-    int allSale;
-    int singlePrice;
-};
+}
+void Invoice::setAllSale(int sale)// the function is to set the sale number
+{
+    if(sale>0)
+    {
+       allSale=sale;
+    }
+    else
+        allSale=0;
 
-
-#endif // INVOICE_H_INCLUDED
+}
+void Invoice::display()// the function is to display the content
+{
+    cout<<"Part's ID:"<<getPartId()<<endl
+    <<"Part's Description:"<<getPartDis()<<endl
+    <<"Sale Number:"<<getAllSale()<<endl
+    <<"Price:"<<getSinglePrice()<<endl;
+    cout<<"Invoice Value:"<<getInvoiceAmount()<<endl<<endl;
+}
+string Invoice::getPartDis()// the function is to set the part description
+{
+    return partDis;
+}
+string Invoice::getPartId()// the function is to get the part id
+{
+    return partId;
+}
+int Invoice::getAllSale()// the function is to get the sale number
+{
+    return allSale;
+}
+int Invoice::getSinglePrice()// the function is to get the single price
+{
+    return singlePrice;
+}
+int Invoice::getInvoiceAmount()// the function is to get the invoice amount
+{
+    return getAllSale()*getSinglePrice();
+}
